98ad5a7f757d56b415bf118bb2d78e0e5d0de76f
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-23 15:11:16 -0700

    finished functionality

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 import smtplib
 import connect
+import pandas
 import datetime as dt
 import random
 
 my_email = connect.ACCOUNT_EMAIL
 password = connect.ACCOUNT_PASSWORD
+now = dt.date.today()
+NUM_EMAIL_TEMPLATES = 3
+PLACEHOLDER = "[NAME]"
 
-now = dt.datetime.now()
-weekday = now.weekday()
+# TODO: 1. Open the birthdays csv
+try:
+    birthdays = pandas.read_csv("birthdays.csv")
+except FileNotFoundError:
+    print("Error opening file")
+    birthday_list = []
+else:
+    birthday_list = birthdays.to_dict(orient="records")
 
-# Check if today matches the day to send message
-if weekday == 3:
-    with open("quotes.txt", "r") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+for n in birthday_list:
+    birthday_month = n["month"]
+    birthday_day = n["day"]
 
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(my_email, password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs="dave@dryweb.com",
-                            msg=f"Subject: Monday Motivation\n\n{quote}"
-                            )
+    if birthday_month == now.month and birthday_day == now.day:
+        print(f"{n['name']} - Match")
+        letter_template = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+        try:
+            with open(letter_template) as letter_file:
+                letter_contents = letter_file.read()
+                stripped_name = n["name"].strip()
+                new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
 
-connection.close()
+                with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+                    connection.starttls()
+                    connection.login(my_email, password)
+                    connection.sendmail(from_addr=my_email,
+                                        to_addrs="dave@dryweb.com",
+                                        msg=f"Subject: Happy Birthday\n\n{new_letter}"
+                                        )
+                    connection.close()
+        except FileNotFoundError:
+            print("No file found.")
