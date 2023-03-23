ac608368b3150b8a258a8223850825c69deebc4f
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-23 15:36:13 -0700

    refactored

--- main.py
+++ main.py
@@ -10,33 +10,30 @@ now = dt.date.today()
 NUM_EMAIL_TEMPLATES = 3
 PLACEHOLDER = "[NAME]"
 
-# TODO: 1. Open the birthdays csv
 try:
     birthdays = pandas.read_csv("birthdays.csv")
 except FileNotFoundError:
     print("Error opening file")
-    birthday_list = []
+    birthday_dic = []
 else:
-    birthday_list = birthdays.to_dict(orient="records")
+    birthday_dic = birthdays.to_dict(orient="records")
 
-for n in birthday_list:
+for n in birthday_dic:
     birthday_month = n["month"]
     birthday_day = n["day"]
 
     if birthday_month == now.month and birthday_day == now.day:
-        print(f"{n['name']} - Match")
-        letter_template = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+        file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
         try:
-            with open(letter_template) as letter_file:
+            with open(file_path) as letter_file:
                 letter_contents = letter_file.read()
-                stripped_name = n["name"].strip()
-                new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+                new_letter = letter_contents.replace(PLACEHOLDER, n["name"].strip())
 
                 with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
                     connection.starttls()
                     connection.login(my_email, password)
                     connection.sendmail(from_addr=my_email,
-                                        to_addrs="dave@dryweb.com",
+                                        to_addrs=n["email"],
                                         msg=f"Subject: Happy Birthday\n\n{new_letter}"
                                         )
                     connection.close()
